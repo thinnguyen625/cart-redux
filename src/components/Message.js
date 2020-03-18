e7f6dd523ac24297a135358e3db909f4b6a6d217
EricNguyen99 <vanthin7111999@gmail.com>
2020-03-18 15:39:05 +0700

    divide components

---
+++ src/components/Message.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h3>
+        <span className="badge amber darken-2">Mua Hàng Thành Công !</span>
+      </h3>
+    );
+  }
+}
+
+export default App;
+
