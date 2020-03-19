36579ddf7198b2914e9e0d6436f6b52777701788
EricNguyen99 <vanthin7111999@gmail.com>
2020-03-19 08:34:55 +0700

    connect to state, obtain data for the product

---
+++ src/reducers/products.js
@@ -0,0 +1,59 @@
+var initState = [
+    {
+        id : 1,
+        name : 'Iphone 11 Pro Max',
+        image : 'https://cdn.tgdd.vn/Products/Images/42/200533/iphone-11-pro-max-green-600x600.jpg',
+        description : 'Sản phẩm do apple sản xuất',
+        price : 500,
+        inventory : 10,
+        rating : 5
+    },
+    {
+        id : 2,
+        name : 'VSmart Active 3',
+        image : 'https://muachonao.com/wp-content/uploads/2020/01/Vsmart-Active-3-6GB-64GB4.jpg',
+        description : 'Sản phẩm do VSmart sản xuất',
+        price : 1500,
+        inventory : 10,
+        rating : 3
+
+    },
+    {
+        id : 3,
+        name : 'Macbook Pro 2020',
+        image : 'https://images-na.ssl-images-amazon.com/images/I/71L2iBSyyOL._SL1500_.jpg',
+        description : 'Sản phẩm do apple sản xuất',
+        price : 2500,
+        inventory : 10,
+        rating : 5
+
+    },
+    {
+        id : 4,
+        name : 'Samsung A71',
+        image : 'https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/642093882.jpeg',
+        description : 'Sản phẩm do Samsung sản xuất',
+        price : 3500,
+        inventory : 10,
+        rating : 4
+
+    },
+    {
+        id : 5,
+        name : 'Mi Band 4',
+        image : 'https://cdn.tgdd.vn/Products/Images/7077/202980/mi-band-4-6-600x600.jpg',
+        description : 'Sản phẩm do Xiaomi sản xuất',
+        price : 4500,
+        inventory : 10,
+        rating : 3
+
+    }
+];
+
+const products = (state = initState, action) => {
+    switch(action.type){
+        default : return [...state];
+    }
+}
+
+export default products;
