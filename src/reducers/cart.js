0c223ce8906c3caa49420700fbc008e54d52f2de
EricNguyen99 <vanthin7111999@gmail.com>
2020-03-22 21:00:59 +0700

    update quanlity product to cart

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,43 +1,39 @@
 import * as Types from './../constants/ActionType';
-//var data = JSON.parse(localStorage.getItem('CART'));
-var initState = [
-    {
-        product: {
-            id: 1,
-            name: 'Iphone 11 Pro Max',
-            image: 'https://cdn.tgdd.vn/Products/Images/42/200533/iphone-11-pro-max-green-600x600.jpg',
-            description: 'Sản phẩm do apple sản xuất',
-            price: 500,
-            inventory: 10,
-            rating: 5
-        },
-        quanlity: 5
-    },
-    {
-        product: {
-            id: 3,
-            name: 'Macbook Pro 2020',
-            image: 'https://images-na.ssl-images-amazon.com/images/I/71L2iBSyyOL._SL1500_.jpg',
-            description: 'Sản phẩm do apple sản xuất',
-            price: 2500,
-            inventory: 10,
-            rating: 5
-        },
-        quanlity: 2
-    }
-];
+var data = JSON.parse(localStorage.getItem('CART'));
+var initState = data ? data : [];
 
 const cart = (state = initState, action) => {
     var { product, quanlity } = action;
+    var index = -1;
     switch (action.type) {
         case Types.ADD_TO_CART:
-            state.push({
-                product,
-                quanlity
-            });
+            index = findProductInCart(state, product);
+            if (index !== -1) {
+                state[index].quanlity += quanlity;
+            } else {
+                state.push({
+                    product,
+                    quanlity
+                });
+            }
+            localStorage.setItem('CART', JSON.stringify(state));
+
             return [...state];
         default: return [...state];
     }
+}
+
+var findProductInCart = (cart, product) => {
+    var index = -1;
+    if (cart.length > 0) {
+        for (var i = 0; i < cart.length; i++) {
+            if (cart[i].product.id === product.id) {
+                index = i;
+                break;
+            }
+        }
+    }
+    return index;
 }
 
 export default cart;
